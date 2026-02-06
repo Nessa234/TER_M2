2646042c14a9d32b7bc260752c4ee875c78dc197
vanessaguerrier <vanessa.guerrier@etu.univ-amu.fr>
2026-02-06 20:46:29 +0100

    first add

---
+++ llama.py
@@ -0,0 +1,10 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+model_name = "openlm-research/open_llama_7b"  # open source
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+prompt = "Write a short story about AI in 2050."
+inputs = tokenizer(prompt, return_tensors="pt")
+output = model.generate(**inputs, max_new_tokens=200)
+print(tokenizer.decode(output[0], skip_special_tokens=True))
